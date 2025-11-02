10bb2d367901e114e33c7f994aab872f2d097977
Linkfinitive <116618526+Linkfinitive@users.noreply.github.com>
2025-11-02 18:24:05 +1100

    Added additional rows to move list view and removed UML class txt file - i have found a way to generate the uml diagram from the code which i will do at the end. This saves me keeping track of all my changes

--- Chess/View/MoveListView.cs
+++ Chess/View/MoveListView.cs
@@ -38,6 +38,20 @@ public class MoveListView : IView
             int xLocation = GlobalSizes.MOVE_LIST_LEFT_OFFSET;
             int yLocation = GlobalSizes.MOVE_LIST_VERTICAL_OFFSET + (i + 2) * GlobalSizes.MOVE_LIST_INTERNAL_VERTICAL_OFFSET;
 
+            //Start the second column of moves after 30 moves
+            if (i >= 30)
+            {
+                xLocation += 4 * GlobalSizes.MOVE_LIST_INTERNAL_HORIZONTAL_OFFSET;
+                yLocation = GlobalSizes.MOVE_LIST_VERTICAL_OFFSET + (i - 28) * GlobalSizes.MOVE_LIST_INTERNAL_VERTICAL_OFFSET;
+            }
+
+            //Or third column if over 60 moves
+            if (i >= 60)
+            {
+                xLocation += 4 * GlobalSizes.MOVE_LIST_INTERNAL_HORIZONTAL_OFFSET;
+                yLocation = GlobalSizes.MOVE_LIST_VERTICAL_OFFSET + (i - 58) * GlobalSizes.MOVE_LIST_INTERNAL_VERTICAL_OFFSET;
+            }
+
             //Move Number
             SplashKit.DrawText($"{i + 1}. ", Theme.MOVE_LIST_TEXT, "arial", 12, xLocation, yLocation);
 
